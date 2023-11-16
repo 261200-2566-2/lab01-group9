05bfaea5f4b6761deb0fd831d2ccc3050d3c1ae0
SUPATKN <yacht1400@gmail.com>
2023-11-16 16:12:53 +0700

    add method

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,27 @@
+import java.util.Scanner;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public static void PrintNonNegInt(int num){
+        if(num < 0){
+            System.out.println("Error it's negative integer!!!");
+
+        }else{
+            for (int i = 1; i <= num; i++) {
+                System.out.println("i = " + i);
+            }
+        }
+
+    }
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        Scanner n = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int number = n.nextInt();
+        PrintNonNegInt(number);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
